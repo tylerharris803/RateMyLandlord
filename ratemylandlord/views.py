a4bc4098d3d2c89c3df2ef2685870c6a5065f373
rcblazz <beccablazzard@gmail.com>
2021-11-08 14:25:24 -0700

    commit by becca

--- ratemylandlord/views.py
+++ ratemylandlord/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+#becca was here
 def indexPageView(request) :
     return HttpResponse('This is the RateMyLandlord homepage')
 
