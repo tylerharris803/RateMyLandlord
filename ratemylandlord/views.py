5bec829e4059ab7a18e2aa15b0c439188e0ac396
tylerharris803 <tylerharris803@gmail.com>
2021-11-08 14:34:58 -0700

    last commit

--- ratemylandlord/views.py
+++ ratemylandlord/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 #Madaleine Was Here lolz
 
-#hey
+#hello there yo
 
 def indexPageView(request) :
     return HttpResponse('This is the RateMyLandlord homepage')
