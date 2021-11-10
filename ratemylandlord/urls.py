fd2a68905dd212539625dbf89dd9f50e04d0095a
tylerharris803 <tylerharris803@gmail.com>
2021-11-10 10:47:50 -0700

    About button commit

--- ratemylandlord/urls.py
+++ ratemylandlord/urls.py
@@ -4,9 +4,9 @@ from .views import viewReviewsPageView
 from .views import editReviewsPageView
 from .views import aboutPageView
 
-urlpatterns = [
-    path("", indexPageView, name="index"),    
+urlpatterns = [    
     path("viewreviews/", viewReviewsPageView, name="viewreviews"),
     path("editreviews/", editReviewsPageView, name="editreviews"),
     path("about/", aboutPageView, name="about"),
+    path("", indexPageView, name="index"),
 ]           
